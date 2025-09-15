f302bfefccc51ed6e07874330b5eb331e4890c88
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-09-15 17:50:38 +0900

    sss

---
+++ jPrac/src/jPrac/MyFrame.java
@@ -0,0 +1,23 @@
+package jPrac;
+import javax.swing.*;
+import java.awt.FlowLayout;
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setSize(300,200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("버튼연습장");
+		setVisible(true);
+		
+		setLayout(new FlowLayout());
+		JButton button=new  JButton("버튼");
+		this.add(button);
+		setVisible(true);
+		setResizable(true);
+	}
+	public static void main(String[] args) {
+		 MyFrame f = new MyFrame();
+		 
+	}
+
+}
+sss
