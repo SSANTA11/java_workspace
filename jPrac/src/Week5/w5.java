d000ef58c09bf26293969e7bc52c4255998050fc
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-10-02 14:49:13 +0900

    5주차 자바 어플리케이션 학습본

--- jPrac/src/Week5/w5.java
+++
@@ -1,5 +0,0 @@
-package Week5;
-
-public class w5 {
-
-}
---
+++ jPrac/src/week4/ActionEventTest2.java
@@ -0,0 +1,7 @@
+package week4;
+
+public class ActionEventTest2 {
+	     public static void main(String[] args) {
+	         MyFrame t = new MyFrame();
+	     }
+	}
