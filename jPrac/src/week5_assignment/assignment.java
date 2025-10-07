3dc12fa29abc313ae0c4bc74d9092e81395778f8
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-10-07 20:40:40 +0900

    이벤트 연습

--- jPrac/src/week5_assignment/assignment.java
+++ jPrac/src/week5_assignment/assignment.java
@@ -9,36 +9,38 @@ public class assignment extends JFrame{
 	private JButton equal;
 	private JLabel plus;
 	private JLabel answer;
-	private int num;
 	public assignment() {
 		setSize(1000,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("HomeWork 1");
 		setLayout(new FlowLayout());
 		
-		plus=new JLabel(Integer.toString(num));
 		num1 = new JTextField(10);
-		num2 = new JTextField(10);
 		plus = new JLabel("+");
+		num2 = new JTextField(10);
+		equal= new JButton("=");
 		answer=new JLabel("");
 		this.add(num1);
 		this.add(plus);
 		this.add(num2);
 		this.add(equal);
 		this.add(answer);
-		setVisible(true);
-		setResizable(true);
-	}
-//    private class MyListener implements ActionListener {
-//        public void actionPerformed(ActionEvent e) {
-//            if (e.getSource() == button1) {
-//           	 number+=1;
-//            }
-//            else {
-//           	 number-=1;
-//            }
-//            label.setText(Integer.toString(number));            	 
-//        }
-//    }
 
+		equal.addActionListener(new MyListener());
+		
+		setVisible(true);
+	}
+    private class MyListener implements ActionListener {	
+    	@Override
+        public void actionPerformed(ActionEvent e) {
+
+        	String n1=num1.getText();
+        	String n2=num2.getText();
+        	int a= Integer.parseInt(n1);
+        	int b= Integer.parseInt(n2);
+        	
+        	answer.setText(Integer.toString(a+b));        
+    }
+
+}
 }
