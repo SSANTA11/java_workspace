d765608c17099d003adac229f3a22e82c378f00b
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-10-10 13:05:58 +0900

    자바어플리케이션 과제1 완성본

--- jPrac/src/week5/W5.java
+++ jPrac/src/week5/W5.java
@@ -1,34 +1,36 @@
 package week5;
 
-
 import java.awt.event.*; // 이벤트 처리를 위한 패키지
 import javax.swing.*;
 
 class MyFrame extends JFrame {
-     private JButton button1, button2;
-     private JLabel label;
-     private int number=1;
-     public MyFrame() {
-         this.setSize(300, 200);
-         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         this.setTitle("이벤트 예제");
-         JPanel panel = new JPanel();
-         label = new JLabel("1");
-         button1 = new JButton("△");
-         button2 = new JButton("▽");
-         button1.addActionListener(new MyListener()); // 버튼에 이벤트 리스너 등록
-         button2.addActionListener(new MyListener()); // 버튼에 이벤트 리스너 등록
+	private JButton button1, button2;
+	private JLabel label;
+	private int number = 1;
 
-         panel.add(label);
-         panel.add(button1);
-         panel.add(button2);
-         
-         this.add(panel);
-         this.setVisible(true);
-     }
+	public MyFrame() {
+		this.setSize(300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("이벤트 예제");
+		JPanel panel = new JPanel();
+		label = new JLabel("1");
+		button1 = new JButton("△");
+		button2 = new JButton("▽");
+		button1.addActionListener(new MyListener());
+		button2.addActionListener(new MyListener());
 
-     private class MyListener implements ActionListener {
-         public void actionPerformed(ActionEvent e) {
+		panel.add(label);
+		panel.add(button1);
+		panel.add(button2);
+
+		this.add(panel);
+		this.setVisible(true);
+	}
+
+	private class MyListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+//        	 try {
+        		 
              if (e.getSource() == button1) {
             	 number+=1;
              }
@@ -36,17 +38,20 @@ class MyFrame extends JFrame {
             	 number-=1;
              }
              label.setText(Integer.toString(number));            	 
+//        	 } catch() {
+//        		 continue;
+//        	 }
          }
-     }
+	}
 }
 
 // 인터페이스란 ?
 // 중간 매개체 역할인 인터페이스.
 
 // 예시 
-	// 컴퓨터에서 마우스 쓰고 싶다.
-	// 운영체제는 사용자와 I/O 사이에서 인터페이스 역할을 한다.
-	// 인터페이스의 용어가 확장되면 플랫폼과 같은 용어들이 등장
+// 컴퓨터에서 마우스 쓰고 싶다.
+// 운영체제는 사용자와 I/O 사이에서 인터페이스 역할을 한다.
+// 인터페이스의 용어가 확장되면 플랫폼과 같은 용어들이 등장
 
 // GUI 
 // 최상위 컨테이너 JFrame
@@ -55,24 +60,23 @@ class MyFrame extends JFrame {
 
 // 생성자의 역할은 데이터 필드에 대한 초기화
 
-
 // C는 함수단위
 // 객체지향은 객체 단위
 
 // 자바의 기본형과 참조형
-	// 정수형 실수형 등
-	// new 연산자 사용은 모두 참조형(또한 객체는 모두 참조형!)
+// 정수형 실수형 등
+// new 연산자 사용은 모두 참조형(또한 객체는 모두 참조형!)
 
 // 접근지정자 4가지 
 
 // 객체 지향 특징3가지
-	// 1. 캡슐화 --> 객체를 만드는 사람과 사용하는 사람 간의 정보 공개 차이(때문에 데이터 필드의 변수는 기본 private, 못해고 protected)
-	// 2. 상속
-	// 3. 다형성 
+// 1. 캡슐화 --> 객체를 만드는 사람과 사용하는 사람 간의 정보 공개 차이(때문에 데이터 필드의 변수는 기본 private, 못해고
+// protected)
+// 2. 상속
+// 3. 다형성
 
 //======================================================================================
 // 5주차 시작(인터페이스) 398pg
-
 
 // 클래스 내에 생성자호출에서 {}와 그 내용 없이 ;으로 마무리한다면 new 가 불가능
 // 인터페이스는 내부에 추상 메소드를 반드시 포함해야한다
@@ -94,17 +98,14 @@ class MyFrame extends JFrame {
 
 // 인터페이스는 다른말로 약속과 같다
 
-
 // 자바의 인터페이스는 다중상속의 장점을 보존하기 위해 만든 것
 
 // 인터페이스의 내부는 메서드의 프로토타입만 존재하다
 // 인터페이스는 멤버 변수를 가질 수 있지만, 그 변수는 오직 public static final 상수여야만 합니다.(?)
 
-
 //무명 클래스
 
 // 인터페이스와 관련있는 람다? **설명은 스킵이지만 중요하니 모두 읽어봐야한다.
-
 
 // 유사한 객체들을 묶어논 것을 패키지라고 한다.
 // 그 예로 javax.swing.*에서 swing이 패키지이다
@@ -116,7 +117,6 @@ class MyFrame extends JFrame {
 // 사용자의 별개 명령이 있을때 까지 대기
 // 이와 관련된 것이 이벤트 구동 프로그래밍
 
-
 // 자바에선 운영체제의 역할을 JVM이 한다. 
 
 // 인터럽트는 소프트웨어나 하드웨어 모두에서 올 수 있다
@@ -127,8 +127,7 @@ class MyFrame extends JFrame {
 // 키보드=keylistener
 
 //456pg 이벤트 리스너를 컴포넌트에 등록한다.-->버튼과 기능을 연결해주는 단계
-	// 이를 button.addActioListener(new MyListener());
-
+// 이를 button.addActioListener(new MyListener());
 
 /// 책 안에 모든 내용이 있기에 인터넷을 애용하자
 // 이르테면 키보드의 리스너가 궁금하면
@@ -140,33 +139,8 @@ class MyFrame extends JFrame {
 // 8장 창만들기와 컴포넌트 넣기
 // 
 
-
-
 // 10장 기능구현
 // 5가지 방법을 모두 숙지해야 한다. 
 // 마지막 방법에서 메서드가 하나만 존재한다면, 람다식으로 표현
 
 // 반드시 람다식은 이해해야한다!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- jPrac/src/week5_assignment/assignment.java
+++ jPrac/src/week5_assignment/assignment.java
@@ -4,43 +4,57 @@ import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.event.*;
 
-public class assignment extends JFrame{
+public class assignment extends JFrame {
 	private JTextField num1, num2;
 	private JButton equal;
-	private JLabel plus;
+	private JRadioButton p, m;
 	private JLabel answer;
+
+	public class PMpanel extends JPanel {
+		public PMpanel() {
+			p = new JRadioButton("+");
+			m = new JRadioButton("-");
+			ButtonGroup group = new ButtonGroup();
+			group.add(p);
+			group.add(m);
+			this.add(p);
+			this.add(m);
+		}
+	}
+
 	public assignment() {
-		setSize(1000,200);
+		setSize(500, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("HomeWork 1");
 		setLayout(new FlowLayout());
-		
 		num1 = new JTextField(10);
-		plus = new JLabel("+");
+		PMpanel radio = new PMpanel();
 		num2 = new JTextField(10);
-		equal= new JButton("=");
-		answer=new JLabel("");
+		equal = new JButton("=");
+		answer = new JLabel("");
 		this.add(num1);
-		this.add(plus);
+		this.add(radio);
 		this.add(num2);
 		this.add(equal);
 		this.add(answer);
 
 		equal.addActionListener(new MyListener());
-		
+
 		setVisible(true);
 	}
-    private class MyListener implements ActionListener {	
-    	@Override
-        public void actionPerformed(ActionEvent e) {
 
-        	String n1=num1.getText();
-        	String n2=num2.getText();
-        	int a= Integer.parseInt(n1);
-        	int b= Integer.parseInt(n2);
-        	
-        	answer.setText(Integer.toString(a+b));        
-    }
+	private class MyListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String n1 = num1.getText();
+			String n2 = num2.getText();
+			int a = Integer.parseInt(n1);
+			int b = Integer.parseInt(n2);
+			if (p.isSelected())
+				answer.setText(Integer.toString(a + b));
+			else if (m.isSelected())
+				answer.setText(Integer.toString(a - b));
+		}
 
-}
+	}
 }
