16ea578b7211fa5b6e75bb5a919f19dfa65cebb5
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-11-03 16:37:02 +0900

    자바 중간발표 준비 및 연습

--- jPrac/src/project_assignment/Prototype.java
+++ jPrac/src/project_assignment/Prototype.java
@@ -1,8 +1,11 @@
 package project_assignment;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,10 +23,47 @@ public class Prototype extends JFrame implements ActionListener{
 	
 	Prototype(){
 		try {
-			img=ImageIO.read(new File(""));
+			img=ImageIO.read(new File("earth.jpg"));
 		}catch(IOException e) {
 			System.out.println("No IMG");
+			System.exit(1);
 		}
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keycode = e.getKeyCode();
+				switch (keycode) {
+				case KeyEvent.VK_UP:
+					img_y-=10;
+					break;
+				case KeyEvent.VK_DOWN:
+					img_y+=10;
+					break;
+				case KeyEvent.VK_RIGHT:
+					img_x+=10;
+					break;
+				case KeyEvent.VK_LEFT:
+					img_x-=10;
+					break;
+				}
+				repaint();
+			}
+		});
+		this.requestFocus();
+		setFocusable(true);
 		
 		setSize(500,800);
 		setTitle("미정");
@@ -33,9 +73,12 @@ public class Prototype extends JFrame implements ActionListener{
 		add(mp);
 		b.addActionListener(this);
 		setVisible(true);
+	}
 		
-		}
-
+	public void paintComponent(Graphics g) {
+		super.paintComponents(g);
+		g.drawImage(img, img_x, img_y, null);
+	}
 	class MP extends JPanel{ // MotherPanel==MP
 		MP(){
 			setLayout(new BorderLayout());;
