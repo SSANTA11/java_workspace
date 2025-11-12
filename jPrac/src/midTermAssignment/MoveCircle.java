dc6cbfe1a922185d60580327158218df0eb5398b
SSANTA11 <ssanta011205@skuniv.ac.kr>
2025-11-12 23:54:07 +0900

    과제물 최종 수정 중간고사 타 반 문제 풀이 완료

--- jPrac/src/midTermAssignment/MoveCircle.java
+++ jPrac/src/midTermAssignment/MoveCircle.java
@@ -1,5 +1,4 @@
 package midTermAssignment;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -8,38 +7,23 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-/*
-
-1. 그림 1을 참고하여 아래 문제를 구현하시오. 
-  - 화면 중앙에 한 개의 원을 그리세요. 
-  - 왼쪽, 오른쪽, 위, 아래, 새로생성 버튼을 만드세요. 
-  - 방향 버튼이 눌러지면 각 방향으로 20픽셀만큼 이동하도록 하세요.
-  - 새로생성 버튼을 클릭하면 화면 가운데 (200, 200) 위치에 새로운 원을 생성합니다. 이후 방향 버튼을 클릭하면 가장 마지막에 생성된 원만 움직입니다. 
-
-
-*/
 public class MoveCircle extends JFrame implements ActionListener {
 	JButton b1 = new JButton("LEFT");
 	JButton b2 = new JButton("RIGHT");
 	JButton b3 = new JButton("UP");
 	JButton b4 = new JButton("DOWN");
 	JButton b5 = new JButton("새로생성");
-
+	P p2 = new P();
 	int arr[][] = new int[10000][2];
-	public static int STOR = 0;
-	public static int x = 200;
-	public static int y = 250;
-
-	public MoveCircle() {
+	int STOR = 0;
+	int x = 200;
+	int y = 250;
+	MoveCircle() {
 		setSize(400, 500);
 		setTitle("Problem 2 : Move Circle");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		JPanel p = new JPanel(new BorderLayout());
 		JPanel p1 = new JPanel();
-		Panel p2 = new Panel();
-
 		p1.add(b1);
 		b1.addActionListener(this);
 		p1.add(b2);
@@ -50,20 +34,12 @@ public class MoveCircle extends JFrame implements ActionListener {
 		b4.addActionListener(this);
 		p1.add(b5);
 		b5.addActionListener(this);
-
 		add(p);
 		p.add(p1, BorderLayout.SOUTH);
 		p.add(p2, BorderLayout.CENTER);
-
 		setVisible(true);
 	}
-
-	public class Panel extends JPanel {
-
-		public Panel() {
-			repaint();
-		}
-
+	class P extends JPanel {
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
@@ -75,48 +51,29 @@ public class MoveCircle extends JFrame implements ActionListener {
 					g.fillOval(a, b, 10, 10);
 				}
 			}
-
 			g.setColor(Color.black);
 			g.fillOval(x, y, 10, 10);
 		}
-
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == b3) {
-			System.out.println("up액션 인식 중 ...");
 			y -= 20;
-			repaint();
 		} else if (e.getSource() == b4) {
-			System.out.println("down액션 인식 중 ...");
-
 			y += 20;
-			repaint();
 		} else if (e.getSource() == b1) {
-			System.out.println("left액션 인식 중 ...");
-
 			x -= 20;
-			repaint();
 		} else if (e.getSource() == b2) {
-			System.out.println("right액션 인식 중 ...");
-
 			x += 20;
-			repaint();
 		} else {
-			System.out.println("새로생성 진입");
 			arr[STOR][0] = x;
 			arr[STOR][1] = y;
 			STOR++;
-
 			x = 200;
 			y = 200;
-
-			repaint();
 		}
-
+		p2.repaint();
 	}
-
 	public static void main(String[] args) {
 		new MoveCircle();
 	}
--- jPrac/src/midTermAssignment/Test1.java
+++ jPrac/src/midTermAssignment/Test1.java
@@ -4,36 +4,29 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-//2. 그림 2는 3진수 덧셈 화면이다. 3진수 입력과 ‘+’, ‘=’은 모두 버튼으로 구현하였다. 예를 들어, 사용자가 1 0 1 + 2 2를 입력한 후,
-//‘=’ 버튼을 클릭하면 3진수 합을 계산하여 계속 출력하도록 한다. 
 
-public class Test1 extends JFrame implements ActionListener {
+
+class Test1 extends JFrame implements ActionListener {
 	JPanel mainP = new JPanel(new BorderLayout());
 	JLabel l = new JLabel("");
-
 	int FLAG = 0;
 	String N1="";
 	String N2="";
-
 	JPanel p = new JPanel();
 	JButton b1 = new JButton("0");
 	JButton b2 = new JButton("1");
 	JButton b3 = new JButton("2");
 	JButton b4 = new JButton("+");
 	JButton b5 = new JButton("=");
-
-	public Test1() {
+	Test1() {
 		setSize(600, 300);
 		setTitle("1번 문제 : 3진수 계산기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		add(mainP);
-
 		p.add(b1);
 		b1.addActionListener(this);
 		p.add(b2);
@@ -44,14 +37,10 @@ public class Test1 extends JFrame implements ActionListener {
 		b4.addActionListener(this);
 		p.add(b5);
 		b5.addActionListener(this);
-
 		mainP.add(l, BorderLayout.NORTH);
 		mainP.add(p, BorderLayout.CENTER);
-
 		setVisible(true);
-
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == b1) {
@@ -68,7 +57,6 @@ public class Test1 extends JFrame implements ActionListener {
 			} else {
 				N2 += "1";
 			}
-
 		} else if (e.getSource() == b3) {
 			l.setText(l.getText() + "2");
 			if (FLAG == 0) {
@@ -76,7 +64,6 @@ public class Test1 extends JFrame implements ActionListener {
 			} else {
 				N2 += "2";
 			}
-
 		} else if (e.getSource() == b4) {
 			if (FLAG == 0) {
 				l.setText(l.getText() + " + ");
@@ -84,15 +71,12 @@ public class Test1 extends JFrame implements ActionListener {
 				System.out.println(N1);
 			}
 		} else {
-
 			int a = Integer.parseInt(N1, 3) + Integer.parseInt(N2, 3);
 			l.setText(l.getText() +"="+Integer.toString(a, 3));
 			FLAG=0;
 		}
 	}
-
 	public static void main(String[] args) {
 		new Test1();
 	}
-
 }
