8ce063a6bfa468892d93fbcb34d59bdd8860c29b
SSANTA11 <ssanta011205@skuniv.ac.kr>
2026-01-09 01:05:04 +0900

    01/08~01/09 중간 학습 일지(백준 브루트 포스) 현재 단계 내 브론즈2 3문제 풀음 내일 이어서 실버3,5,4 해결 예정

--- Practice/src/pp/Main.java
+++ Practice/src/pp/Main.java
@@ -1,40 +1,56 @@
+// package pp;
+// import java.util.Scanner;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
+//         int d = sc.nextInt();
+//         int e = sc.nextInt();
+//         int f = sc.nextInt();
+//         for (int x = 0; x <= 999; x++) {
+//             for (int y = 0; y <= 999; y++) {
+//                 if (x * a + b * y == c && x * d + e * y == f) {
+//                     System.out.println(x + " " + y);
+//                 } else if ((-1) * x * a + b * y == c && (-1) * x * d + e * y == f) {
+//                     System.out.println((-1) * x + " " + y);
+//                 } else if (x * a + b * (-1) * y == c && x * d + e * (-1) * y == f) {
+//                     System.out.println(x + " " + (-1) * y);
+//                 } else if ((-1) * x * a + b * (-1) * y == c && (-1) * x * d + e * (-1) * y == f) {
+//                     System.out.println((-1) * x + " " + (-1) * y);
+//                 }
+//             }
+//         }
+//         sc.close();
+//     }
+// }
 package pp;
 
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-//		int nums[] = new int[3], max = 0, sum = 0;
-//		Scanner sc = new Scanner(System.in);
-//		for (int i = 0; i < 3; i++) {
-//			nums[i] = sc.nextInt();
-//		}
-//		while (true) {
-//			for (int i = 0; i < 3; i++) {
-//				if (nums[i] > nums[max]) {
-//					max = i;
-//				}
-//			}
-//			for (int i = 0; i < 3; i++) {
-//				if (i != max) {
-//					sum += nums[i];
-//				}
-//			}
-//			if (sum <= nums[max]) {
-//				nums[max] -= 1;
-//				max = 0;
-//				sum = 0;
-//			} else {
-//				sum += nums[max];
-//				System.out.println(sum);
-//				break;
-//			}
-//		}
-//	}
-		int nums[] = new int[3], max = 0, sum = 0;
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 3; i++) {
-			nums[i] = sc.nextInt();
-		}
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int i = 0;
+        char board[][] = new char[N][M];
+        for (int h = 0; h < N; h++) {
+            String str = sc.nextLine();
+            for (char e : str.toCharArray()) {
+                board[h][i] = e;
+                i++;
+            }
+            i = 0;
+        }
+        for (int h = 1; h < N; h++) {
+            for (int w = 1; w < M; w++) {
+                char tmp = board[h - 1][w - 1];
+                if ()
+            }
+        }
+        sc.close();
+    }
 }
